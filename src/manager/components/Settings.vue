ee58dac1e233194b0e79807a894a1daca7ffc1c6
Marcus Mu <chunkhang@gmail.com>
2018-04-11 00:59:12 +0800

    Enable about

--- src/manager/components/Settings.vue
+++ src/manager/components/Settings.vue
@@ -24,7 +24,7 @@ export default {
           handle: this.handleAbout,
           title: "",
           active: false,
-          disabled: true
+          disabled: false
         }
       },
       text: "Coming soon."
